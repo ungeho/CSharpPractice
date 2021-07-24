13fa2f5fb71eab720497429b4c1c8439c07b159b
tsumugiw <85925233+tsumugiw@users.noreply.github.com>
2021-07-24 10:21:11 +0900

    m9

---
+++ chap06/regularexpression/regularexpression/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace regularexpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tel = new[] {"080-0000-0000", "084-0000-0000" , "184-0000" };
+            var rgx = new Regex(@"\d{2,4}-\d{2,4}-\d{2,4}");
+            foreach(var t in tel)
+            {
+                Console.WriteLine(rgx.IsMatch(t) ? t : "アンマッチ");
+            }
+        }
+    }
+}
